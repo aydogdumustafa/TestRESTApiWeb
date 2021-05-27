576a045fef8066ae19dbe23ed0351e985f793ddd
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-27 05:56:42 +0200

    Add project files.

---
+++ TestRESTApiWeb/Controllers/StudentDataController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestRESTApiWeb.Models;
+
+namespace TestRESTApiWeb.Controllers
+{
+    public class StudentDataController : ApiController
+    {
+        // GET: api/StudentData
+        public List<StudentDemoData> Get()
+        {
+            StudentDataList sdList = new StudentDataList();
+            return sdList.studentDemoDatas; 
+        }
+
+        // GET: api/StudentData/5
+        public StudentDemoData Get(int id)
+        {
+            StudentDemoData sdReturn = null;
+            StudentDataList sdList = new StudentDataList();
+            foreach (StudentDemoData sd in sdList.studentDemoDatas)
+            {
+                if (sd.RollNum == id)
+                {
+                    sdReturn = sd;
+                    break;
+                }
+            }
+            return sdReturn;
+        }
+
+        // POST: api/StudentData
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/StudentData/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/StudentData/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestRESTApiWeb/Models/StudentDemoData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestRESTApiWeb.Models
+{
+    public class StudentDemoData
+    {
+        public int RollNum { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+
+    }
+
+    public class StudentDataList 
+    {
+        public List<StudentDemoData> studentDemoDatas { get; set; } = null;
+        public StudentDataList()
+        {
+            studentDemoDatas = new List<StudentDemoData>();
+            StudentDemoData sd = new StudentDemoData();
+            sd.RollNum = 1001;
+            sd.Name = "Mustafa";
+            sd.Course = "MCA";
+            studentDemoDatas.Add(sd);
+
+            sd = new StudentDemoData();
+            sd.RollNum = 1002;
+            sd.Name = "Ali";
+            sd.Course = "MDA";
+            studentDemoDatas.Add(sd);
+
+            sd = new StudentDemoData();
+            sd.RollNum = 1003;
+            sd.Name = "Emre";
+            sd.Course = "BAL";
+            studentDemoDatas.Add(sd);
+
+        }
+
+    }
+
+
+}
